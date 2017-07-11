7283f81186176f3a14f29a9d8f8bb82e10f2ec30
Arumoy Shome <arumoy.shome@gmail.com>
2017-07-11 19:37:30 -0400

    ⚠️  Problem 4 WIP.

--- lib/problem_4.rb
+++ lib/problem_4.rb
@@ -1,3 +1,19 @@
-def is_palindrome?(word)
-  word == word.reverse
+def is_palindrome?(num)
+  num.to_s == num.to_s.reverse
+end
+
+def largest_palindrome
+  first = 10
+  last = 99
+
+  while last >= first do
+    puts "LAST: #{last} FIRST: #{first}"
+    product = first * last
+    puts "PRODUCT: #{product}"
+
+    return product if is_palindrome?(product)
+
+    first += 1
+    last -= 1
+  end
 end
--- test/problem_4_test.rb
+++ test/problem_4_test.rb
@@ -1,8 +1,15 @@
-require 'minitest/autorun'
-require '../problem_4/problem_4'
+require 'test_helper'
 
 class Problem4Test < Minitest::Test
-  def test_is_palindrome_returns_true_when_word_is_a_palindrome
-    assert is_palindrome?("1001")
+  def test_is_palindrome_returns_true_when_num_is_a_palindrome
+    assert is_palindrome?(1001)
+  end
+
+  def test_is_palindrome_returns_false_when_num_is_not_a_palindrome
+    refute is_palindrome?(12345)
+  end
+
+  def test_largest_palindrome_returns_the_largest_palindrome_from_product_of_3_digit_numbers
+    assert_equal 0, largest_palindrome
   end
 end
